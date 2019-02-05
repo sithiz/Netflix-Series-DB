9634a1a8815f51faeeb92cd7901f127c891c0bc1
Sithiz <Joshklaas1@gmail.com>
2019-02-05 14:21:09 -0600

    Routes for get and post completed

--- models/router.js
+++ models/router.js
@@ -1,11 +1,62 @@
+const express = require('express')
+const router = express.Router()
 const { Client } = require('pg')
+
+
 const connectionString = 'postgresql://sithis@localhost:5432/netflix_series'
 const client = new Client({
-	connectionString: connectionString,
+    connectionString: connectionString,
 })
 client.connect()
 
-client.query('SELECT * FROM shows',(error,response)=>{
-	console.log(response.rows)
-	client.end()
+//ALL SHOW AND INFORMTION
+router.get('/Shows', (request, response) => {
+    client.query('SELECT show_title,series_length,bio FROM shows', (error, result) => {
+        response.send(result.rows)
+    })
 })
+router.get('/Shows/:id', (request, response) => {
+    let id = request.params.id
+    client.query('SELECT show_title,series_length,bio FROM shows', (error, result) => {
+        response.send(result.rows[id])
+    })
+})
+//ALL ACTORS 
+router.get('/Actors', (request, response) => {
+    client.query('SELECT actor_name FROM actors', (error, results) => {
+        response.send(results.rows)
+    })
+})
+
+router.get('/Actors/:id', (request, response) => {
+    let id = request.params.id
+    client.query('SELECT actor_name FROM actors', (error, results) => {
+        response.send(results.rows[id])
+    })
+})
+router.get('/All', (request, response) => {
+    client.query('SELECT * FROM shows_actors', (error, results) => {
+        response.send(results.rows)
+    })
+})
+
+router.post('/NewSeries',(request,response)=>{
+		let id = (request.body['ID']*Math.random())*10000003
+		let show_title = request.body['show_title']
+		let series_length = request.body['series_length']
+		let bio = request.body['bio']
+		client.query(`INSERT INTO shows VALUES (${id}, ${show_title},${series_length},${bio})`) 
+		client.query('SELECT * FROM shows').then((result)=>{
+			response.send(result.rows)
+		})
+	})
+
+		
+
+
+router.post('/')
+
+router.delete('/')
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
 const PORT = 9000
+const netflixRouter = require('./models/router.js')
+const bodyParser= require('body-parser')
 
-
-
-
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/netflix', netflixRouter)
 
 
 
