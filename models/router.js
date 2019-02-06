26c7e71866c1e7a87dba655bf487c56f29e6f948
Sithiz <Joshklaas1@gmail.com>
2019-02-06 11:14:59 -0600

    Made one route more secure

--- models/router.js
+++ models/router.js
@@ -2,55 +2,86 @@ const express = require('express')
 const router = express.Router()
 const { Client } = require('pg')
 
-
 const connectionString = 'postgresql://sithis@localhost:5432/netflix_series'
 const client = new Client({
     connectionString: connectionString,
 })
+let errorMiddleware = function(error){
+    console.log(error.message, 'OH no you broke my cheese')
+    client.end()
+}
+
 client.connect()
 
 //ALL SHOW AND INFORMTION
 router.get('/Shows', (request, response) => {
-    client.query('SELECT show_title,series_length,bio FROM shows', (error, result) => {
-        response.send(result.rows)
-    })
+    client.query('SELECT show_title,series_length,bio FROM shows')
+        .then((result) => {
+            response.send(result.rows)
+        })
+        .catch((error)=>{
+            errorMiddleware(error)
+        })
 })
 router.get('/Shows/:id', (request, response) => {
     let id = request.params.id
-    client.query('SELECT show_title,series_length,bio FROM shows', (error, result) => {
-        response.send(result.rows[id])
-    })
+    client.query('SELECT show_title,series_length,bio FROM shows')
+        .then((result) => {
+            response.send(result.rows[id])
+        })
+        .catch((error)=>{
+            errorMiddleware(error)
+        })
 })
 //ALL ACTORS 
 router.get('/Actors', (request, response) => {
-    client.query('SELECT actor_name FROM actors', (error, results) => {
-        response.send(results.rows)
-    })
+    client.query('SELECT actor_name FROM actors')
+        .then((result) => {
+            response.send(result.rows)
+        })
+        .catch((error)=>{
+            errorMiddleware(error)
+        })
 })
 
 router.get('/Actors/:id', (request, response) => {
     let id = request.params.id
-    client.query('SELECT actor_name FROM actors', (error, results) => {
-        response.send(results.rows[id])
-    })
+    client.query('SELECT actor_name FROM actors')
+        .then((result) => {
+            response.send(result.rows[id])
+        })
+        .catch((error)=>{
+            errorMiddleware(error)
+        })
 })
 router.get('/All', (request, response) => {
     client.query('SELECT * FROM shows_actors')
-        .then((result)=>{
+        .then((result) => {
             response.send(result.rows)
-    })
+        })
+        .catch((error)=>{
+            errorMiddleware(error)
+        })
 })
 
 router.post('/NewSeries', (request, response) => {
     let id = (request.body['ID'] * Math.random()) * 10000003
-    let show_title = request.body['show_title']
-    let series_length = request.body['series_length']
-    let bio = request.body['bio']
-    client.query(`INSERT INTO shows VALUES (${id}, ${show_title},${series_length},${bio})`)
+    let data = {
+        show_title:request.body.show_title,
+        series_length:request.body.series_length,
+        bio:request.body.bio
+    }
+    console.log(data)
+
+    client.query(`INSERT INTO shows VALUES (${id}, $1,$2,$3)`,
+        [data.show_title,data.series_length,data.bio])
     client.query('SELECT * FROM shows')
-    .then((result) => {
-        response.send(result.rows)
-    })
+        .then((result) => {
+            response.send(result.rows)
+        })
+        .catch((error)=>{
+            errorMiddleware(error)
+        })
 })
 
 router.post('/NewActors', (request, response) => {
@@ -58,39 +89,52 @@ router.post('/NewActors', (request, response) => {
     let actorName = request.body['Actors_name']
     client.query(`INSERT INTO Actors VALUES (${id}, ${actorName})`)
     client.query('SELECT * FROM Actors')
-    .then((result) => {
-        response.send(result.rows)
-    })
+        .then((result) => {
+            response.send(result.rows)
+        })
+        .catch((error)=>{
+            errorMiddleware(error)
+        })
 
 })
 
-router.put('/Shows/:id',(request,response)=>{	
-	let id = request.params.id
-	client.query(`UPDATE Shows SET Show_title='NewStuff', Series_Length=12 WHERE ID = ${id}`)
-	client.query(`SELECT * FROM shows WHERE ID = ${id}`)
-	.then((result)=>{
-		response.send(result.rows)
-	})
+router.put('/Shows/:id', (request, response) => {
+    let id = request.params.id
+    client.query(`UPDATE Shows SET Show_title='NewStuff', Series_Length=12 WHERE ID = ${id}`)
+    client.query(`SELECT * FROM shows WHERE ID = ${id}`)
+        .then((result) => {
+            response.send(result.rows)
+        })
+        .catch((error)=>{
+            errorMiddleware(error)
+        })
 
 })
-router.put('/Actors/:id',(request,response)=>{	
-	let id = request.params.id
-	client.query(`UPDATE Shows SET Show_title='NewStuff', Series_Length=12 WHERE ID = ${id}`)
-	client.query(`SELECT * FROM shows WHERE ID = ${id}`)
-	.then((result)=>{
-		response.send(result.rows)
-	})
+router.put('/Actors/:id', (request, response) => {
+    let id = request.params.id
+    client.query(`UPDATE Actors SET Actors_name='Ron Swanson' WHERE ID = ${id}`)
+    client.query(`SELECT * FROM shows WHERE ID = ${id}`)
+        .then((result) => {
+            response.send(result.rows)
+        })
+        .catch((error)=>{
+            errorMiddleware(error)
+        })
 
 })
 
-router.delete('/Actors/:id',(request,response)=>{
-	let id = request.params.id
-	client.query(`DELETE FROM Actors WHERE ID=${id}`)
-	client.query(`SELECT * FROM Actors`)
-	.then((result)=>{
-		response.send(result.rows)
-	})
+router.delete('/Actors/:id', (request, response) => {
+    let id = request.params.id
+    client.query(`DELETE FROM Actors WHERE ID=${id}`)
+    client.query(`SELECT * FROM Actors`)
+        .then((result) => {
+            response.send(result.rows)
+        })
+        .catch((error)=>{
+            errorMiddleware(error)
+        })
 })
+ 
 
 
 module.exports = router
