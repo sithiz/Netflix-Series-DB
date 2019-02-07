fedd0fb74ab6c4affd0a9fab68c15b3e196f0ea1
Sithiz <Joshklaas1@gmail.com>
2019-02-07 09:25:23 -0600

    starting last once over before submitting

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const app = express()
 const PORT = 9000
 const netflixRouter = require('./models/router.js')
 const bodyParser= require('body-parser')
+const morgan = require('morgan')
 
+app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use('/netflix', netflixRouter)
 
+app.use('/', netflixRouter)
 
 
 app.listen(PORT, ()=> {
