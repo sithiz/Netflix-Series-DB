2fb6e672d860398ac97d7b80ecf9247577c80885
Sithiz <Joshklaas1@gmail.com>
2019-02-07 11:00:08 -0600

    ADDED COMMENTS

--- models/router.js
+++ models/router.js
@@ -1,19 +1,36 @@
+/************************************************************
+************ Required Modules for Router to work ************
+************************************************************/
+
 const express = require('express')
 const router = express.Router()
 const { Client } = require('pg')
 
-const connectionString = 'postgresql://sithis@localhost:5432/netflix_series'
-const client = new Client({
-    connectionString: connectionString,
-})
+/*************************************************************
+************** Error Handingling *****************************
+*************************************************************/
 let errorMiddleware = function(error){
     console.log(error.message, 'OH no you broke my cheese')
     client.end()
 }
 
-client.connect()
+            /****************************************
+            Connecntion point and beginning of routes
+            ****************************************/
+const connectionString = 
+'postgresql://sithis@localhost:5432/netflix_series' ;
 
-//ALL SHOW AND INFORMTION
+const client = new Client({
+    connectionString: connectionString,
+});
+
+ client.connect()
+
+/*****************************************************************
+******************** GET Routes **********************************
+*****************************************************************/
+           
+            /****** ALL SHOWS *******/
 router.get('/Shows', (request, response) => {
     client.query('SELECT show_title,series_length,bio FROM shows')
         .then((result) => {
@@ -23,6 +40,8 @@ router.get('/Shows', (request, response) => {
             errorMiddleware(error)
         })
 })
+
+            /****** SHOWS BY ID *****/
 router.get('/Shows/:id', (request, response) => {
     let id = request.params.id
     client.query('SELECT show_title,series_length,bio FROM shows')
@@ -33,7 +52,8 @@ router.get('/Shows/:id', (request, response) => {
             errorMiddleware(error)
         })
 })
-//ALL ACTORS 
+
+            /***** ALL ACTORS *****/ 
 router.get('/Actors', (request, response) => {
     client.query('SELECT actor_name FROM actors')
         .then((result) => {
@@ -43,7 +63,7 @@ router.get('/Actors', (request, response) => {
             errorMiddleware(error)
         })
 })
-
+            /***** ACTORS BY ID *****/
 router.get('/Actors/:id', (request, response) => {
     let id = request.params.id
     client.query('SELECT actor_name FROM actors')
@@ -54,6 +74,7 @@ router.get('/Actors/:id', (request, response) => {
             errorMiddleware(error)
         })
 })
+        /***** ALL ACTORS IN THERE SHOWS *****/
 router.get('/All', (request, response) => {
     client.query('SELECT * FROM shows_actors')
         .then((result) => {
@@ -64,6 +85,11 @@ router.get('/All', (request, response) => {
         })
 })
 
+/***********************************************************
+***************** ALL POST ROUTES **************************
+***********************************************************/
+
+            /***** POSTING NEW SERIES *****/
 router.post('/NewSeries', (request, response) => {
     let id = (request.body['ID'] * Math.random()) * 10000003
     let data = {
@@ -71,7 +97,6 @@ router.post('/NewSeries', (request, response) => {
         series_length:request.body.series_length,
         bio:request.body.bio
     }
-    console.log(data)
     client.query(`INSERT INTO shows VALUES (${id}, $1,$2,$3)`,
         [data.show_title,data.series_length,data.bio])
     client.query(`SELECT * FROM shows WHERE id = ${id}`)
@@ -82,6 +107,8 @@ router.post('/NewSeries', (request, response) => {
             errorMiddleware(error)
         })
 })
+
+            /***** POSTING NEW ACTORS *****/
 
 router.post('/NewActors', (request, response) => {
     let id = (request.body['ID'] * Math.random()) * 10000003
@@ -101,16 +128,22 @@ router.post('/NewActors', (request, response) => {
 
 })
 
+/*********************************************************
+****************** ALL PUT ROUTES ************************
+*********************************************************/
+
+            /***** UPDATING SHOW *****/
 router.put('/Shows/:id', (request, response) => {
-    let id = request.params.id
+    request.params.id
     let data = {
+        id:request.params.id,
         show_title:request.body.show_title,
         series_length: request.body.series_length,
         bio:request.body.bio
     }
-    client.query(`UPDATE Shows SET show_title=($1), series_Length=($2), bio=($3) WHERE ID = ${id}`,
-        [data.show_title,data.series_length,data.bio])
-    client.query(`SELECT * FROM shows WHERE ID = ${id}`)
+    client.query('UPDATE Shows SET show_title=($1), series_Length=($2), bio=($3) WHERE ID = ($4)',
+        [data.show_title,data.series_length,data.bio,data.id])
+    client.query('SELECT * FROM shows WHERE ID = ($1)',[data.id])
         .then((result) => {
             response.send(result.rows)
         })
@@ -119,10 +152,18 @@ router.put('/Shows/:id', (request, response) => {
         })
 
 })
-router.put('/Actors/:id', (request, response) => {
-    let id = request.params.id
-    client.query(`UPDATE Actors SET Actors_name='Ron Swanson' WHERE ID = ${id}`)
-    client.query(`SELECT * FROM shows WHERE ID = ${id}`)
+            /***** UPDATING ACTORS *****/
+router.put('/Actor/:id', (request, response) => {
+    let data = {
+        id:request.params.id,
+        newActor:request.params.newActor
+    }
+    client
+        .query('UPDATE Actors SET Actors_name=($1) WHERE ID = ($2)',
+            [data.newActor,data.id])
+    client
+        .query('SELECT * FROM shows WHERE ID =($1)',
+            [data.id])
         .then((result) => {
             response.send(result.rows)
         })
@@ -132,6 +173,11 @@ router.put('/Actors/:id', (request, response) => {
 
 })
 
+/******************************************************************
+********************* DELETE ROUTE *******************************
+******************************************************************/
+
+        /******** DELETE BY ACTOR ID ********/
 router.delete('/Actors/:id', (request, response) => {
     let id = request.params.id
     client.query(`DELETE FROM Actors WHERE ID=${id}`)
@@ -144,6 +190,6 @@ router.delete('/Actors/:id', (request, response) => {
         })
 })
  
-
+        /****** EXPORTING ROUTER ******/
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,25 +1,39 @@
-const express = require('express')
-const app = express()
-const PORT = 9000
-const netflixRouter = require('./models/router.js')
-const bodyParser= require('body-parser')
-const morgan = require('morgan')
-const fs = require('fs')
-const path = require('path')
 
+/*****************************************************************
+*****************THE IMPORTED MODULES ***************************/
+		const express = require('express')        
+		const app = express()					  
+		const PORT = 9000						
+		const netflixRouter = require('./models/router.js')
+		const bodyParser= require('body-parser') 
+		const morgan = require('morgan')	
+		const fs = require('fs')				
+		const path = require('path')			
+/*****************************************************************
+*****************************************************************/
+
+		/*********** LOGGER SET UP ************/
 var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
 
-
 app.use(morgan('combined', { stream: accessLogStream }))
+/********************************************************
+****************** BODY PARSER SETUP ********************
+********************************************************/
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+//body parser adds a body to request object 
 
-app.use('/', netflixRouter)
+	/***************** ROUTER ************************/
+			app.use('/', netflixRouter)
 
 
-app.listen(PORT, ()=> {
-	console.log('Listening at port: ' + PORT)
-})
+/*****************************************************/
+/******************	LISTENING PORT 	 *****************/	
+		
+		app.listen(PORT, ()=> {
+			console.log('Listening at port: ' + PORT)
+		})
 
 
 
